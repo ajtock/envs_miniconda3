aac73e3c6daa9c888fda608a7a19e44a6efa2790
ajtock <andytock@gmail.com>
2021-09-17 12:19:27 +0100

    wget HiC-Pro

---
+++ HiC_Pro/wget_HiC-Pro-3.0.0.tar.gz.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# See https://github.com/nservant/HiC-Pro for full installation instructions
+# (e.g., including dependencies installed in a conda environment)
+# https://github.com/nservant/HiC-Pro/releases/tag/v3.0.0
+
+wget -O HiC-Pro-3.0.0.tar.gz https://github.com/nservant/HiC-Pro/archive/v3.0.0.tar.gz
